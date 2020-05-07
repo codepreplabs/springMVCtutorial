fbba4ea3137097af13f5fd9fc438ddfaed027bb1
praveen <thejavamonk@gmail.com>
2020-05-07 12:25:32 +0530

    example sprimg MVC app using spring boot

---
+++ 02bootMVC/src/main/java/com/codeprep/bootmvc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.codeprep.bootmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(Model model){
+        model.addAttribute("message", "Spring MVC using spring boot, hello world!!");
+        return "hello";
+    }
+}
